7c52ec1b3a0024486aa0990d44ef48544460cba3
timurseta <timur_seta@hotmail.com>
2021-09-24 23:44:09 +0200

    nested arrays

---
+++ ex05/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+var myPetsArray = [
+    {animalType:"Dog",
+    name: "Pujdo"
+    },
+    {animalType:"Cat",
+    name: "Maca"
+    },
+    {animalType:"Bird",
+    name: "Tweety"
+    },  
+]
+function myPetsFunction(pets) {
+    return pets.name;
+}
+// Only change code above this line
+
+console.log(myPetsFunction(myPetsArray[1]));
+module.exports = myPetsFunction;
+module.exports.myPets = myPetsArray;
