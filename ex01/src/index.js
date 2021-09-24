736f9a6c48c88c7e0427e856f2464b4105508f49
timurseta <timur_seta@hotmail.com>
2021-09-24 21:54:03 +0200

    recursion sum of arrays

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function myRecursion(arr, n) {
+    if (n <= 0) {
+        return 0;
+    } else {
+        return (myRecursion(arr, n -1) + arr[n - 1]);
+    }
+}
+// Only change code above this line
+
+console.log(myRecursion([1, 2, 3, 4, 5, 6], 5));
+module.exports = myRecursion;
